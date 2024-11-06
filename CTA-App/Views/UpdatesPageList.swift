27d01d6cd4353bdd512244b87e4a32a1ccd3c853
Kevin Retzloff <kretzloff04@gmail.com>
2024-11-06 17:50:35 -0600

    Add files via upload

---
+++ CTA-App/Views/UpdatesPageList.swift
@@ -0,0 +1,29 @@
+//
+//  UpdatesPageList.swift
+//  CTA Practice2.0
+//
+//  Created by 2 GO Participant on 11/6/24.
+//
+
+import SwiftUI
+
+struct UpdatesPageList: View {
+        var body: some View {
+            NavigationSplitView {
+                List(Updates) { update in
+                    NavigationLink {
+                        UpdatesDetails(update: update)
+                    } label: {
+                        UpdatesPageRow(updatespage: update)
+                    }
+                }
+                .navigationTitle("Updates")
+            } detail: {
+                Text("Select an update")
+            }
+        }
+    }
+
+    #Preview {
+        UpdatesPageList()
+    }
